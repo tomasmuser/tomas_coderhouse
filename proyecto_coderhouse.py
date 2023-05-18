6bf946b78b69a720c2ba0261820fe894fd801855
tomasmuser <tomas.muser@gmail.com>
2023-05-18 06:36:10 -0300

    Reformating

--- proyecto_coderhouse.py
+++ proyecto_coderhouse.py
@@ -9,12 +9,10 @@ key = keys.ver(303)
 
 url="https://api.weatherbit.io/v2.0/history/daily?lat={0}&lon={1}&start_date={2}&end_date={3}&key={4}"
 
+# Manhattan
 latitude = 40.776676
 longitude = -73.971321
 
-year = 2022
-month = 1
-day = 1
 
 # Dictionary to transform all dtypes to usable types
 dic_types = {
@@ -74,11 +72,13 @@ def conection():
         password= keys.ver(202),
         port="5439")
     return conn
-
 # Table creation.
-def create_table(table_name, df):
+def create_table(table_name):
+
     cursor.execute("DROP TABLE IF EXISTS daily_weather")
+    print(" Table droped")
     columns = []
+    df = apic(start_date, end_date)
     for column in df.columns:
         data_type = dic_pdtosql[str(df[column].dtype)]
         columns_sql = '{} {}'.format(column, data_type)
@@ -86,8 +86,7 @@ def create_table(table_name, df):
 
     table_query = "CREATE TABLE {} ({})".format(table_name, ", ".join(columns))
     cursor.execute(table_query)
-
-
+#
 def apic(start_date, end_date):
         end_date = start_date + timedelta(1)
         full_url = url.format(latitude,longitude,start_date,end_date,key)
@@ -96,28 +95,28 @@ def apic(start_date, end_date):
         json_data = json.loads(response.text, parse_constant=lambda x: x.replace("'", '"'))
         df = pd.json_normalize(json_data, record_path =['data'])
         df = df.astype(dic_types)
+        print("df normalizado")
         return df
-
+#
 def intert_data(start_date, end_date, table_name):
     fin = end_date
     while fin >= start_date:
         df = apic(start_date, end_date,)
+        print(start_date, end_date)
         add_row(table_name, df)
         start_date = start_date + timedelta(1)
-        time.sleep(10)
-        print(end_date)
-
-
+        time.sleep(5)
+#
 def add_row(table_name, df):
     row = df.iloc[0]
     column = ', '.join(row.index)
     values = ', '.join(['%({0})s'.format(col) for col in row.index])
     query = "INSERT INTO {} ({}) VALUES ({})".format(table_name, column, values)
-
+    print("preparado")
     cursor.execute(query, row)
+    print("listo")
 
-
-start_date = date(year,month,day) # 2023-04-04
+start_date = date(2022,1,1) # 2023-04-04
 end_date = date(2022,12,31) # Request required furter date
 table_name = "daily_weather"
 
@@ -126,7 +125,10 @@ conn = conection()
 cursor = conn.cursor()
 
 with cursor:
-    create_table(table_name, df=apic(start_date, end_date))
+    print("Creating {} table".format(table_name))
+    #create_table(table_name)
+    print("Table {} successfully created.".format(table_name))
+    print("Initializing data insertion")
     intert_data(start_date, end_date, table_name)
 
 conn.commit()
